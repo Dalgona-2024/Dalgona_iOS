2da977367befff2210ec8bc34811963ddce4337e
Byung jin <bjcho1503@naver.com>
2024-07-16 15:33:31 +0900

    🎨 :: 디자인 시스템 구현

---
+++ App/Resources/Color+.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+extension UIColor {
+
+    // MARK: - main
+    static let main = DalgonaIOSAsset.main.color
+
+    // MARK: - point
+    static let point1 = DalgonaIOSAsset.point1.color
+    static let point2 = DalgonaIOSAsset.point2.color
+    static let point3 = DalgonaIOSAsset.point3.color
+
+    // MARK: - gray
+    static let gray50 = DalgonaIOSAsset.gray50.color
+    static let gray100 = DalgonaIOSAsset.gray100.color
+    static let gray200 = DalgonaIOSAsset.gray200.color
+    static let gray300 = DalgonaIOSAsset.gray300.color
+    static let gray400 = DalgonaIOSAsset.gray400.color
+    static let gray500 = DalgonaIOSAsset.gray500.color
+    static let gray600 = DalgonaIOSAsset.gray600.color
+    static let gray700 = DalgonaIOSAsset.gray700.color
+    static let gray800 = DalgonaIOSAsset.gray800.color
+    static let gray900 = DalgonaIOSAsset.gray900.color
+}
---
+++ App/Resources/Font/Font+.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+extension UIFont {
+    // MARK: - Bold
+    static let bold40 = DalgonaIOSFontFamily.MangoDdobak.bold.font(size: 40)
+    static let bold32 = DalgonaIOSFontFamily.MangoDdobak.bold.font(size: 32)
+    static let bold24 = DalgonaIOSFontFamily.MangoDdobak.bold.font(size: 24)
+    static let bold18 = DalgonaIOSFontFamily.MangoDdobak.bold.font(size: 18)
+
+    //MARK: - Regular
+    static let regular32 = DalgonaIOSFontFamily.MangoDdobak.bold.font(size: 32)
+    static let regular24 = DalgonaIOSFontFamily.MangoDdobak.bold.font(size: 24)
+    static let regular18 = DalgonaIOSFontFamily.MangoDdobak.bold.font(size: 18)
+    static let regular16 = DalgonaIOSFontFamily.MangoDdobak.bold.font(size: 16)
+    static let regular14 = DalgonaIOSFontFamily.MangoDdobak.bold.font(size: 14)
+    static let regular12 = DalgonaIOSFontFamily.MangoDdobak.bold.font(size: 12)
+}
